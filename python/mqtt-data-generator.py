3a5c2bcf491317966bdfea47e98140eb6a8b30de
ANISH ARALIKATTI <anisharalikatti24@gmail.com>
2019-06-23 17:44:53 +0530

    Create mqtt-data-generator.py

---
+++ python/mqtt-data-generator.py
@@ -0,0 +1,53 @@
+import paho.mqtt.client as mqttClient
+import time
+import random
+import time
+from timeloop import Timeloop
+from datetime import timedelta
+ 
+def on_connect(client, userdata, flags, rc):
+ 
+    if rc == 0:
+        print("Connected to broker")
+        global Connected                
+        Connected = True                 
+    else:
+        print("Connection failed")
+ 
+Connected = False   
+ 
+broker_address= "m24.cloudmqtt.com"
+port = 10174
+user = "zwfxzaip"
+password = "8rQqi99icKka"
+ 
+client = mqttClient.Client("Python")               
+client.username_pw_set(user, password=password)    
+client.on_connect= on_connect                      
+client.connect(broker_address, port=port)          
+ 
+client.loop_start()        
+ 
+while Connected != True:    
+    time.sleep(0.1)
+ 
+try:
+    tl = Timeloop()
+
+    @tl.job(interval=timedelta(seconds=1))
+    def sample_job_every_10s():
+        temp = random.uniform(15,30)
+        humidity = random.uniform(20,50)
+        pressure = random.uniform(1.0,1.2)
+        val_list = [temp, humidity, pressure]
+        print(val_list)
+        client.publish("python/test",temp)
+        client.publish("python/test",humidity)
+        client.publish("python/test",pressure)
+        
+    tl.start(block=True)
+ 
+except KeyboardInterrupt:
+ 
+    client.disconnect()
+    client.loop_stop()
