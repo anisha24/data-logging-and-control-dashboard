06f737a7e2acbb1c6dcc5f4398874e8ccbdfe4a2
anisha24 <anisharalikatti24@gmail.com>
2019-07-09 16:56:36 +0530

    END OF DAY COMMIT

--- angular-front-end/src/app/app-routing.module.ts
+++ angular-front-end/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ConfgaccountComponent } from './confgaccount/confgaccount.component';
+import { VisualisationComponent } from './visualisation/visualisation.component';
 
 const routes: Routes = [
 { path: 'login', component: LoginComponent},
 { path: 'register', component: RegisterComponent},
 { path: 'dashboard', component: DashboardComponent},
+{ path: 'config', component: ConfgaccountComponent},
+{ path: 'visual', component: VisualisationComponent},
 { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
